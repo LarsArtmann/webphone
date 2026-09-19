e29e7a0ed183acdfbf10f2ea4b95ae04ff21db66
Lars Artmann <git@lars.software>
2026-09-19 19:19:18 +0200

    chore: auto-commit 1 changed file(s) (heuristic)

--- flake.nix
+++ flake.nix
@@ -220,11 +220,9 @@
                   name = "hsts-opt-in";
                   path = pkgs.writeText "hsts-opt-in" (
                     let
-                      hstsEvaluated = lib.evalModules (
-                        moduleSet {
-                          services.webphone.nginx.hsts.enable = true;
-                        }
-                      );
+                      hstsEvaluated = lib.evalModules (moduleSet {
+                        nginx.hsts.enable = true;
+                      });
                       hstsVhost = hstsEvaluated.config.services.nginx.virtualHosts."phone.example.org";
                     in
                     if lib.hasInfix "Strict-Transport-Security" hstsVhost.extraConfig then
